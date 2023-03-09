12370199e2e3e586ce93ced68dd5e3cf79d90b0a
Circlemeow <931720936@qq.com>
2023-03-09 14:58:20 +0000

    Updated 120s options

--- chip/system2/stimulus.sv
+++ chip/system2/stimulus.sv
@@ -1,13 +1,13 @@
 
 //------------------------------------------------------------------------------
 //  Titile: System module - 2022/2023 SubFile: Stimulus
-//  Author: Clark Pu, Chen Paiyun
+//  Author: Clark Pu, Paiyun Chen (Circle)
 //    Team: C4 Chip Designed
 // Version: 2.0 Initial Behavioural Simulation
 // Stimulus Process
 //------------------------------------------------------------------------------
 
-`define use_ahb_monitor
+//`define use_ahb_monitor
 `define ingore_read_flag
 
 //------------------------------------------------------------------------------
@@ -540,17 +540,17 @@ initial begin
 
   PressModeButtonTest;
   #5s;
-  $display("/n This is trip time. And real trip time is %ds:/n", trip_time);
+  $display("\n This is trip time. And real trip time is %ds:\n", trip_time);
   DisplaySegment;
 
   PressModeButtonTest;
   #5s;
-  $display("/n This is speed:");
+  $display("\n This is speed:");
   DisplaySegment;
 
   PressModeButtonTest;
   #5s;
-  $display("/n This is cadence:");
+  $display("\n This is cadence:");
   DisplaySegment;
 
   #5s;
@@ -559,9 +559,16 @@ initial begin
   OdometerVerification;
   DisplaySegment;
 
+  #50s;
+
   PressModeButtonTest;
   #5s;
-  $display("/n This is trip time. And real trip time is %ds:/n", trip_time);
+  $display("\n This is trip time. And real trip time is %ds:\n", trip_time);
+  DisplaySegment;
+
+  PressModeButtonTest;
+  #5s;
+  $display("\n This is trip time. And real trip time is %ds:\n", trip_time);
   DisplaySegment;
 
   #5s;
