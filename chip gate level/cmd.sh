46685959a65e226462f17a47770e4afa13738979
masterpu2020 <1378161935@qq.com>
2023-03-16 20:45:48 +0000

    增加了综合命令的自动初始化

--- chip gate level/cmd.sh
+++ chip gate level/cmd.sh
@@ -36,7 +36,8 @@ echo " > Select Command:"
 echo "1.Compile Software."
 echo "2.Simulation (Gate Level)."
 echo "3.Simulation without Graphic (Gate Level)."
-echo "4.DOS Format Check."
+echo "4.Synthesis."
+echo "5.DOS Format Check."
 echo "------------------------------------------"
 echo " Enter Number to Run. Enter Any Key Else to Quit."
 read -p "Enter: " choice
@@ -53,6 +54,26 @@ elif [ "$choice" = "3" ]; then
 	echo -e "\n------------------------------------------\n Processing...\n"
 	./simulate -no_graphics -gate gate_level -sdf computer.sdf 
 elif [ "$choice" = "4" ]; then
+	echo -e "\n------------------------------------------\n Processing...\n"
+	cd ./synthesis
+	init=0
+	for files in `ls -a`
+	do
+		if [ "$files" = ".synopsys_dc.setup" ]; then
+			init=1
+		fi
+	done
+	if [ "$init" = "0" ]; then
+		echo " > Seems you didn't setup, do you want to setup now?"
+		echo "   y for Yes, Enter Any Key Else for No."
+		read -p "Enter: " choice
+		if [ "$choice" = "y" ]; then
+			do_c35b4_copy_synopsys_setup
+		fi
+	fi
+	dc_shell -gui
+	cd ..
+elif [ "$choice" = "5" ]; then
 	echo -e "\n------------------------------------------\n Processing...\n"
 	echo "Check folder chip"
 	format
