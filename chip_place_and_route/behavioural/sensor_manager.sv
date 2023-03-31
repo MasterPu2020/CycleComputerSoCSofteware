3a7057e63e4a2003c8a55cd87c46d4536fd20c88
masterpu2020 <1378161935@qq.com>
2023-03-31 20:06:33 +0100

    修复了低速不准的问题，更新了新版OLED，但暂时无法使用

--- chip_place_and_route/behavioural/sensor_manager.sv
+++ chip_place_and_route/behavioural/sensor_manager.sv
@@ -3,7 +3,7 @@
 // Titile:  Sensor Manager Behavioural
 // Author:  Clark Pu
 // Team:    C4 Chip Designed
-// Version: 3.0
+// Version: 3.1
 // Verification: Verified
 // Comment: Redesigned
 //------------------------------------------------------------------------------
@@ -36,25 +36,22 @@ timeunit 1ns; timeprecision 100ps;
 //------------------------------------------------------------------------------
 // Memory Map:
 // 6000_0000: 32bit | Recording triggerd fork cycle number
-// 6000_0004: 32bit | Recording triggerd delta crank cycle number
-// 6000_0008: 32bit | Recording triggerd delta fork cycle number
-// 6000_000A: 32bit | Recording triggerd delta crank time
-// 6000_0010: 32bit | Recording triggerd delta fork time
+// 6000_0004: 32bit | Recording triggerd 1 cycle crank time
+// 6000_0008: 32bit | Recording triggerd 1 cycle fork time
 //------------------------------------------------------------------------------
 
 logic [15:0] fork_data;
-logic [ 7:0] delta_crank_data, delta_fork_data;
 logic [15:0] crank_time, fork_time;
 
 //------------------------------------------------------------------------------
 // Control and Status registers
 //------------------------------------------------------------------------------
 
-enum logic [2:0] {Idle, ReadDeltaCrank, ReadDeltaFork, ReadDeltaCrankTime, ReadDeltaForkTime, ReadFork, WriteFork} control;
+logic ahb_write;
+logic [1:0] ahb_addr;
+
 logic last_fork, last_crank;
-logic clear_delta_fork, clear_delta_crank;
 logic [ 5:0] time_counter;
-logic [15:0] sample_crank_time, sample_fork_time;
 logic [15:0] delta_crank_time, delta_fork_time;
 
 logic  SYNC_MID_nFork,  SYNC_nFork;
@@ -71,29 +68,19 @@ localparam
 
 always_ff @(posedge HCLK, negedge HRESETn) begin
   if (!HRESETn) begin
-    control <= Idle;
-    crank_time <= 0;
-    fork_time <= 0;
+    ahb_write <= 0;
+    ahb_addr <= 0;
   end
   else begin
-    if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 0) &&  HWRITE)
-      control <= WriteFork;
-    else if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 0) && !HWRITE)
-      control <= ReadFork;
-    else if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 1) && !HWRITE) begin
-      control <= ReadDeltaCrank;
-      crank_time <= sample_crank_time; // Special: Save sample time for next reading operation
+    if (HREADY && HSEL && (HTRANS != NoTransfer)) begin
+      ahb_write <= HWRITE;
+      ahb_addr <= HADDR[3:2];
     end
-    else if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 2) && !HWRITE) begin
-      control <= ReadDeltaFork;
-      fork_time <= sample_fork_time; // Special: Save sample time for next reading operation
+    else begin
+      ahb_write <= 0;
+      ahb_addr <= 0;
     end
-    else if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 3) && !HWRITE) 
-      control <= ReadDeltaCrankTime;
-    else if (HREADY && HSEL && (HTRANS != NoTransfer) && (HADDR[4:2] == 4) && !HWRITE) 
-      control <= ReadDeltaForkTime;
-    else
-      control <= Idle;
+    
   end
 end
 
@@ -119,17 +106,13 @@ end
 always_ff @(posedge HCLK, negedge HRESETn) begin
   if (!HRESETn) begin
     fork_data <= 0;
-    delta_crank_data <= 0;
-    delta_fork_data <= 0;
     delta_crank_time <= 0;
     delta_fork_time <= 0;
     time_counter <= 0;
-    sample_fork_time <= 0;
-    sample_crank_time <= 0;
-    clear_delta_fork <= 0;
-    clear_delta_crank <= 0;
     last_fork <= 0;
     last_crank <= 0;
+    crank_time <= 0;
+    fork_time <= 0;
   end
   else begin
     //--------------------------------------------------------------------------
@@ -139,8 +122,14 @@ always_ff @(posedge HCLK, negedge HRESETn) begin
     // Inner Timer: Count by 1 millisecond
     if (time_counter == Count1MS) begin 
       time_counter <= '0;
-      delta_fork_time <= delta_fork_time + 1;
-      delta_crank_time <= delta_crank_time + 1;
+      if (delta_crank_time > 3000)
+        crank_time <= delta_crank_time;
+      else
+        delta_crank_time <= delta_crank_time + 1;
+      if (delta_fork_time > 3000)
+        fork_time <= delta_fork_time;
+      else
+        delta_fork_time <= delta_fork_time + 1;
     end
     else
       time_counter <= time_counter + 1;
@@ -150,40 +139,24 @@ always_ff @(posedge HCLK, negedge HRESETn) begin
       last_fork <= 1;
     else if (last_fork) begin
       fork_data <= fork_data + 1;
-      if (clear_delta_fork) begin // Clear fork at the hall point
-        delta_fork_data <= 0;
-        delta_fork_time <= 0;
-        clear_delta_fork <= 0;
-      end
-      else begin
-        delta_fork_data <= delta_fork_data + 1;
-        sample_fork_time <= delta_fork_time;
-      end
+      fork_time <= delta_fork_time;
+      delta_fork_time <= 0;
       last_fork <= 0;
     end
     if (!SYNC_nCrank)
       last_crank <= 1;
     else if (last_crank) begin
-      if (clear_delta_crank) begin // Clear crank at the hall point
-        delta_crank_data <= 0;
-        delta_crank_time <= 0;
-        clear_delta_crank <= 0;
-      end
-      else begin
-        delta_crank_data <= delta_crank_data + 1;
-        sample_crank_time <= delta_crank_time;
-      end
+      crank_time <= delta_crank_time;
+      delta_crank_time <= 0;
       last_crank <= 0;
     end
-
     //--------------------------------------------------------------------------
     // One Cycle Delayed AHB Control
     //--------------------------------------------------------------------------
-    case (control)
-      ReadDeltaCrank : begin delta_crank_data <= 0; clear_delta_crank <= 1; end
-      ReadDeltaFork  : begin delta_fork_data  <= 0; clear_delta_fork  <= 1; end
-      WriteFork      : fork_data <= HWDATA[15:0];
-    endcase
+
+    if (ahb_write && ahb_addr == 0)
+      fork_data <= HWDATA;
+
   end
 end
 
@@ -194,13 +167,13 @@ end
 always_comb begin
   HREADYOUT = '1;
   HRDATA = 32'b0;
-  case (control)
-    ReadDeltaCrank     : HRDATA = delta_crank_data;
-    ReadDeltaCrankTime : HRDATA = crank_time;
-    ReadDeltaFork      : HRDATA = delta_fork_data;
-    ReadDeltaForkTime  : HRDATA = fork_time;
-    ReadFork           : HRDATA = fork_data;
-  endcase
+  if (!ahb_write) begin
+    case (ahb_addr)
+      0 : HRDATA = fork_data;
+      1 : HRDATA = crank_time;
+      2 : HRDATA = fork_time;
+    endcase
+  end
 end
 
 endmodule
